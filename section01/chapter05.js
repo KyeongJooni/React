a4f447082cb2246330e958462a81bec297f42327
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-03-14 16:42:39 +0900

    Second commit

---
+++ section01/chapter05.js
@@ -0,0 +1,27 @@
+//1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let minf = -Infinity;
+
+let nan = NaN;
+
+//2. String Type
+let myName = "이경준";
+let myLocation = "성남";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 있습니다`;
+// 템플릿 리터럴 문법
+
+//3. Boolean Type
+let SwitchOn = true;
+let SwitchOff = false;
+
+//4. Null Type
+let empty = null; //명시적으로 정의해줘야 함
+
+//5. Undefined Type
+let none; //자동으로 값이 들어감
