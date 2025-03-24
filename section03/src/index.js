b65bba6e1a9cf2b05a22d8a58512760e70c995db
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-03-24 21:19:04 +0900

    31th commit

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,9 +1,12 @@
 //const moduleData = require("./math");
-import { add, sub } from "./math.js"; //동일한 경로에서 불러오기 때문에 아래랑 합치는 거도 가능
-import mul from "./math.js";
+import mul, { add, sub } from "./math.js"; //동일한 경로에서 불러오기 때문에 아래랑 합치는 거도 가능
+import randomColor from "randomcolor";
 //console.log(moduleData.add(1, 2));
 //console.log(moduleData.sub(1, 2));
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+//console.log(add(1, 2));
+//console.log(sub(1, 2));
+//console.log(mul(2, 3));
+
+const color = randomColor();
+console.log(color);
