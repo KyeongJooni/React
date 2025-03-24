42f21a3fb971c5cd76c51f683ef2c21097d2aa27
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-03-24 15:58:14 +0900

    30th commit

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,9 @@
-console.log("안녕");
+//const moduleData = require("./math");
+import { add, sub } from "./math.js"; //동일한 경로에서 불러오기 때문에 아래랑 합치는 거도 가능
+import mul from "./math.js";
+//console.log(moduleData.add(1, 2));
+//console.log(moduleData.sub(1, 2));
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
---
+++ section03/src/math.js
@@ -0,0 +1,16 @@
+//math모듈
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+//module.exports = { add, sub };
+
+//export { add, sub };
+
+export default function multiply(a, b) {
+  return a * b;
+}
