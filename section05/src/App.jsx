3383b6eff200ba8b7103815fd6b4218fd009d0d2
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-03-25 13:55:16 +0900

    34th commit

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
+//vite로 만든것은 파일 확장자 안써도 된다
+//함수의 첫글자는 무조건 대문자
 function App() {
+  //부모 컴포넌트
   return (
     <>
-      <h1>안녕 리액트</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
