3a6da85cc5f11b7c9225d35e0cfa1a9d361a8856
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-03-28 17:47:06 +0900

    38th commit

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,30 +1,38 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import { useState } from "react";
 
 //vite로 만든것은 파일 확장자 안써도 된다
 //함수의 첫글자는 무조건 대문자
+// useState를 안쓰면 리렌더링이 되지 않음
 function App() {
   //부모 컴포넌트
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
   return (
     //스프레드 연산자
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
-}
+} //컴포넌트의 값이 바뀌면 return을 다시 반환한다
 
 export default App;
