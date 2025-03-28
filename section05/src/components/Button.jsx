53243f03b6a839b2b0a456886407ed991d40059f
KyeongJooni <165972574+KyeongJooni@users.noreply.github.com>
2025-03-28 17:23:02 +0900

    37th commit

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,17 @@
 const Button = ({ children, text, color = "black" }) => {
   //구조분해 할당 문법 사용, 기본값 설정
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      //onMouseEnter={onClickButton} //이벤트 핸들러
+      style={{ color: color }}
+    >
       {text}-{color.toUpperCase()}
       {children}
     </button>
